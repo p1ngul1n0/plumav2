d7742ea01883d698fbc3be02c2252dc9738d7caa
sampzzz <57546870+sampzzz@users.noreply.github.com>
2021-03-06 20:03:39 -0300

    Update results.js

--- static/js/results.js
+++ static/js/results.js
@@ -11,14 +11,26 @@ $(document).ready(function() {
 
     $('.open-details').click(function() {
         $('#response-text').text($(this).closest('td').next('.response-td').text());
-        console.log($(this).closest('td'));
         $('#request-id').text($(this).closest('td').prevAll('.response-id').first().text());
         $('#request-payload').text($(this).closest('td').prevAll('.response-payload').first().text());
         $('#request-status').text($(this).closest('td').prevAll('.response-status').first().text());
+        findString()
         $('#modal').toggle();
+        
     })
 
     $('#close-modal').click(function() {
         $('#modal').toggle();
     })
+
+    $('#search-input').keyup(function () {
+        findString();
+        
+    })
+    function findString () {
+        $('#response-text').unhighlight();
+        $('#response-text').highlight($('#search-input').val());
+        $('#search-results').text($('.highlight').length + ' results.')
+    }
+
 });
