f28ede9c4c22747ceb3fcfae8c589bb8ecc6525c
sampzzz <57546870+sampzzz@users.noreply.github.com>
2021-03-06 18:23:39 -0300

    Update results.js

--- static/js/results.js
+++ static/js/results.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
+    $('.enum-table').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
     $('.response-button').click(function() {
         $(this).closest('tr').next('.response-tr').toggle();
+    })
+
+    $('.open-details').click(function() {
+        $('#response-text').text($(this).closest('td').next('.response-td').text());
+        console.log($(this).closest('td'));
+        $('#request-id').text($(this).closest('td').prevAll('.response-id').first().text());
+        $('#request-payload').text($(this).closest('td').prevAll('.response-payload').first().text());
+        $('#request-status').text($(this).closest('td').prevAll('.response-status').first().text());
+        $('#modal').toggle();
+    })
+
+    $('#close-modal').click(function() {
+        $('#modal').toggle();
     })
 });
